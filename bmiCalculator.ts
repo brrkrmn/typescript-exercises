8faa48ddeb1c5127d74c866b90fcc9f0c047af0a
Berra Karaman <esma.berra.karaman@hotmail.com>
2023-05-16 10:08:28 +0300

    bmi is calculated from command line

---
+++ bmiCalculator.ts
@@ -0,0 +1,42 @@
+interface BmiValues {
+    height: number;
+    weight: number;
+}
+
+const parseArguments = (args: string[]): BmiValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            height: Number(args[2]),
+            weight: Number(args[3])
+        }
+    } else {
+        throw new Error('Provided values are not numbers!');
+    }
+}
+
+const calculateBmi = (h: number, w: number) => {
+    const bmi: number = (w / h / h) * 10000
+    if (bmi < 18.5) {
+        console.log('underweight')
+    } else if ( bmi < 24.9 ) {
+        console.log('normal')
+    } else if (bmi < 29.9) {
+        console.log('overweight')
+    } else {
+        console.log('obese')
+    }
+}
+
+try {
+    const { height, weight } = parseArguments(process.argv)
+    calculateBmi(height, weight)
+} catch (error: unknown) {
+    let errorMessage = 'Something went wrong.'
+    if (error instanceof Error) {
+        errorMessage += 'Error: ' + error.message;
+    }
+    console.log(errorMessage)
+}
