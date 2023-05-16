2120b6b2a738fe6a9ca0aa9037b77f8714a825aa
Berra Karaman <esma.berra.karaman@hotmail.com>
2023-05-16 12:38:55 +0300

    exerciseCalculator works from command line

---
+++ exerciseCalculator.ts
@@ -0,0 +1,66 @@
+interface exerciseResults {
+    peirodLength: number;
+    trainingDays: number;
+    success: boolean;
+    rating: number;
+    ratingDescription: string;
+    target: number;
+    average: number;
+}
+
+const parseExercises = (args: string[]): number[] => {
+    if (args.length !== 9) throw new Error('Please provide 7 days');
+    let exercises: number[] = []
+    args.slice(2).map(day => {
+        if(!isNaN(Number(day))) {
+            exercises.push(Number(day))
+        } else {
+            throw new Error('Please provide exercise hours as numbers.')
+        }
+    })
+    return exercises
+}
+
+const calculateExercise = (exercises: number[]): exerciseResults => {
+    const target = 3
+    const totalExercisedDays: number = exercises.filter(day => day !== 0).length
+    const totalExercisedHours: number = exercises.reduce((sum, day) => sum + day, 0) 
+    const success = totalExercisedDays >= target
+    const average = totalExercisedHours/7
+    
+    let rating = 0
+    let ratingDescription = ''
+    if(average < 1) {
+        rating = 1
+        ratingDescription = 'Work harder!'
+    } else if (average < 2) {
+        rating = 2
+        ratingDescription = 'Keep Going!'
+    } else {
+        rating = 3
+        ratingDescription = 'Great Result!'
+    }
+
+    const results: exerciseResults = {
+        peirodLength: exercises.length,
+        trainingDays: totalExercisedDays,
+        success: success,
+        rating: rating,
+        ratingDescription: ratingDescription,
+        target: target,
+        average: average,
+    }
+    
+    return(results)
+}
+
+try {
+    const exercises = parseExercises(process.argv)
+    console.log(calculateExercise(exercises))
+} catch (error: unknown) {
+    let errorMessage = 'Something went wrong.'
+    if (error instanceof Error) {
+        errorMessage += 'Error: ' + error.message;
+    }
+    console.log(errorMessage)
+}
